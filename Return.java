407cc5ec0d8c4274a929836746cc6ce45227ce76
Dvnhrdt <harditiadovan@gmail.com>
2021-10-18 14:02:23 +0700

    first commit

---
+++ Return.java
@@ -0,0 +1,21 @@
+public class Return{
+
+    public static void main(String[] args) {
+        
+        Nilairatarata(1200);
+        
+    }
+
+    static void Nilairatarata(int angka){
+        System.out.println("Dovan Harditia");
+        if(angka< 70){
+            System.out.println("Anda Tidak Lulus");
+        
+        if(angka> 100)
+            System.out.println("Nilai Salah");
+        }else{
+            System.out.println("Anda Lulus");
+        }
+    } 
+}       
+ 

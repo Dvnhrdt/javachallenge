cb69aff66b7df073e7edab03eda918f967f96aaf
Dvnhrdt <91587246+Dvnhrdt@users.noreply.github.com>
2021-10-18 14:09:26 +0700

    Add files via upload

---
+++ javamethod.java
@@ -0,0 +1,22 @@
+public class javamethod{
+
+    public static void main(String[] args) {
+        
+        Nilairatarata(101);
+        
+    }
+
+    static void Nilairatarata(int angka){
+        System.out.println("Dovan Harditia");
+        if(angka < 70){
+            System.out.println("Anda Tidak Lulus");
+        
+        }else if(angka > 100){
+            System.out.println("Nilai Salah");
+
+        }else {
+            System.out.println("Anda Lulus");
+        }
+    } 
+}       
+ 
